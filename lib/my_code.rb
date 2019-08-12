99c9e4251d4fd3737815ecd7eb852cb35351c652
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-12 16:19:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,3 +11,11 @@ end
 def map_to_no_change(source_array)
   return source_array
 end
+
+def map_to_double(source_array)
+  i = 0
+  while i < source_array.length do
+    source_array[i] *= 2
+  end
+  return source_array
+end
